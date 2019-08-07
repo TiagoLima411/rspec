fd92a8aa29c956b6f365e5a4e7d9f0db9f5ae54d
= <=>
2019-08-07 14:15:09 -0300

    Mocks Expectations

--- spec/mocks/mocks_spec.rb
+++ spec/mocks/mocks_spec.rb
@@ -12,4 +12,28 @@ describe 'Mocks' do
     #exercise
     student.bar
   end
+
+  it 'args' do
+      student = Student.new
+      expect(student).to receive(:foo).with(123)
+      student.foo(123)
+  end
+
+  it 'repetição' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).twice
+    #expect(student).to receive(:foo).with(123).once
+    #expect(student).to receive(:foo).with(123).exactly(3).times
+    #expect(student).to receive(:foo).with(123).at_least(:once)
+    #expect(student).to receive(:foo).with(123).at_least(:twice)
+    #expect(student).to receive(:foo).with(123).at_least(:n).times
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it 'retorno' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).and_return(true)
+    student.foo(123)    
+  end
 end
