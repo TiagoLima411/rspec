0c13d78654574f3b44f636f2126ae001cdc95a23
= <=>
2019-08-07 14:22:00 -0300

    as_null_object

--- spec/test_doubles/user_spec.rb
+++ spec/test_doubles/user_spec.rb
@@ -1,10 +1,19 @@
 describe 'Test Double' do
   it '__' do
-      user = double('User')
-      #allow(user).to receive_messages(name: "Tiago", password: 'secret')
-      allow(user).to receive(:name).and_return('Tiago')
-      allow(user).to receive(:password).and_return('secret')
-      user.name
-      user.password      
+    user = double('User')
+    #allow(user).to receive_messages(name: "Tiago", password: 'secret')
+    allow(user).to receive(:name).and_return('Tiago')
+    allow(user).to receive(:password).and_return('secret')
+    user.name
+    user.password      
+  end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive(:name).and_return('Tiago')
+    allow(user).to receive(:password).and_return('secret')
+    puts user.name
+    puts user.password      
+    user.abc # <= null object
   end
 end
